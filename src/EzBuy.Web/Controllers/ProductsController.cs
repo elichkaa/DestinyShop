b1d5b40b8adaa39348dc3f48accdf41a0acc421d
elichka <ed44362072@edu.mon.bg>
2022-03-01 16:34:39 +0200

    Add basic product management menu

---
+++ src/EzBuy.Web/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EzBuy.Web.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+
+namespace EzBuy.Web.Controllers
+{
+    [Authorize]
+    public class ProductsController : Controller
+    {
+        public ILogger<ProductsController> _logger { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Add()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Edit()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Delete()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
