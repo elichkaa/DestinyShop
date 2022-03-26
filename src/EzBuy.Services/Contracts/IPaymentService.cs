675857b613b3656d4ecc0fdb2601d78b8ee58d48
ZVidenov <zhanvidenov59@gmail.com>
2022-03-26 17:23:33 +0200

    Payment service interface

--- src/EzBuy.Services/Contracts/IPaymentService.cs
+++ src/EzBuy.Services/Contracts/IPaymentService.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using EzBuy.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace EzBuy.Services.Contracts
 {
     public interface IPaymentService
     {
-
+        string ToUrlEncodedString(Dictionary<string, string> request);
+        Dictionary<string, string> ToDictionary(string response);
+        bool AddTransaction(Dictionary<string, string> request, string payRequestId);
+        bool UpdateTransaction(Dictionary<string, string> request, string payRequestId);
+        Transaction GetTransaction(string payRequestId);
+        string GetMd5Hash(Dictionary<string, string> data, string encryptionKey);
+        bool VerifyMd5Hash(Dictionary<string, string> data, string encryptionKey, string hash);
+        User GetUser();
     }
 }
